b7ba0b70859a550eed602e8441a523f759395b40
Moise <mbrebe@g.educaand.es>
2023-10-28 18:51:43 +0200

    los botones funcionan, menos el calcular

--- app/src/main/java/com/example/actividad_04_pmdm/Activity_imc.kt
+++ app/src/main/java/com/example/actividad_04_pmdm/Activity_imc.kt
@@ -9,30 +9,70 @@ import com.google.android.material.slider.RangeSlider
 
 class Activity_imc : AppCompatActivity() {
     val buttonIdList = listOf(R.id.edadMinus,R.id.edadPlus,R.id.pesoMinus,R.id.pesoPlus)
+    var peso = 0
+    var edad = 0
+    var altura = 0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
-        val heightTextView = findViewById<TextView>(R.id.sliderTextView)
-        val slider = findViewById<RangeSlider>(R.id.slider)
 
-
-        slider.addOnChangeListener{_,value,_->
-            heightTextView.text = "${value.toInt()} cm"
+        for (button in buttonIdList){
+            val floatingbutton = findViewById<FloatingActionButton>(button)
+            floatingbutton.setOnClickListener { buttons(floatingbutton) }
         }
 
 
 
+        altura = slider()
+
 
     }
-//    fun buttons(button: FloatingActionButton){
-//        when(button.id){
-//            R.id.edadMinus ->{
-//
-//            }
-//        }
-//    }
 
+    fun slider(): Float {
+        val heightTextView = findViewById<TextView>(R.id.sliderTextView)
+        val slider = findViewById<RangeSlider>(R.id.slider)
+        var altura = 0F
+        slider.valueTo = 220F
+        slider.valueFrom = 120.0F
+        slider.values = listOf(120F)
 
+        slider.addOnChangeListener{_,value,_->
+            heightTextView.text = "${value.toInt()} cm"
+            altura = value
+        }
+        return altura
+    }
+
+   fun buttons(button: FloatingActionButton){
+       val edadTextView = findViewById<TextView>(R.id.edadTextView)
+       val pesoTextView = findViewById<TextView>(R.id.pesoTextView )
+           when(button.id){
+               R.id.edadMinus ->{
+                    edad = edadTextView.text.toString().toInt()
+                    edadTextView.text = if(edad > 1){
+                        (edad-1).toString()
+                    }else{
+                        edad.toString()
+                    }
+                }
+               R.id.edadPlus->{
+                   edad = edadTextView.text.toString().toInt()
+                   edadTextView.text = (edad+1).toString()
+               }
+               R.id.pesoMinus ->{
+                   peso = pesoTextView.text.toString().toInt()
+                   pesoTextView.text = if(peso > 1){
+                       (peso-1).toString()
+                   }else{
+                       peso.toString()
+                   }
+               }
+               R.id.pesoPlus->{
+                   peso = pesoTextView.text.toString().toInt()
+                   pesoTextView.text = (peso+1).toString()
+               }
+        }
+    }
 }
