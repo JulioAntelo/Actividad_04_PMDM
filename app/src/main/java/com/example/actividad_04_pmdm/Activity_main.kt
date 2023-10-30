ef646ecf5d4afdc89d4db38648547bf74774ba83
AdrianPRG <alopgal962@g.educaand.es>
2023-10-30 15:45:33 +0100

    a

--- app/src/main/java/com/example/actividad_04_pmdm/Activity_main.kt
+++ app/src/main/java/com/example/actividad_04_pmdm/Activity_main.kt
@@ -3,7 +3,7 @@ package com.example.actividad_04_pmdm
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class Main_Activity:AppCompatActivity() {
+class Activity_main:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
